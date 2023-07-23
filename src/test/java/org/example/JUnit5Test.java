69c557a79e5c857eaa71e0e6bc223bfc4c02e8e4
Masaru Nomura <massa.nomura@gmail.com>
2023-07-24 02:17:17 +0900

    Add test with JUnit5 dept

---
+++ src/test/java/org/example/JUnit5Test.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class JUnit5Test { // Class can be either public or package-private.
+    @Test
+    void test() { // Method can be either public or package-private.
+        assertTrue(true);
+    }
+}
