ce01cfe83f233a88172c356437342e7fa523e92c
Masaru Nomura <massa.nomura@gmail.com>
2023-07-28 01:29:11 +0900

    Add test with randomized value with 0 operand

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -21,6 +21,20 @@ public class CalculatorTest {
         @Nested
         @DisplayName("where the sum doesn't overflow")
         class WhereSumDoesNotOverflow {
+            @Nested
+            @DisplayName("given an identity element (0) as a param")
+            class GivenIdentityElement {
+                @Test
+                void returnsSum() {
+                    int expected = between(Integer.MIN_VALUE, Integer.MAX_VALUE);
+                    int actual = calculator.add(0, expected);
+                    int actual2 = calculator.add(expected, 0);
+
+                    assertEquals(expected, actual);
+                    assertEquals(expected, actual2);
+                }
+            }
+
             @Test
             void returnsSum() {
                 int actual = calculator.add(1, 1);
@@ -38,5 +52,19 @@ public class CalculatorTest {
                         () -> calculator.add(Integer.MAX_VALUE, 1));
             }
         }
+    }
+
+    /**
+     * Returns an integer between {@code min} and {@code max}.
+     *
+     * @param min minimum value of the range.
+     * @param max maximum value of the range.
+     * @return
+     */
+    private static int between(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("");
+        }
+        return (int) ((Math.random() * ((long) max - (long) min)) + (long) min);
     }
 }
